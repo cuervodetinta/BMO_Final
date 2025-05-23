205213a7afa969e3d5cc3ff4555400e5c5ae77b1
Mateo Acosta <twishm08@gmail.com>
2025-05-23 17:36:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,11 @@ st.set_page_config(page_title="BMO interactivo", layout="centered")
 st.sidebar.title("Funciones Disponibles")
 pagina = st.sidebar.radio("Ir a", ["Saludo", "Control de Baile", "Chatea con BMO"])
 
-# Página: Inicio
+# Página: Saludo
 if pagina == "Saludo":
     st.title("Saludo de BMO")
-    st.write("Usa el menú lateral para controlar dispositivos y reproducir sonidos.")
+    st.write("Presiona el boton para que BMO te salude.")
+    #APARTIR DE AQUI EMPIEZA A PROGRAMAR EL SALUDO
 
 # Página: Control de Baile
 elif pagina == "Control de Baile":
@@ -35,17 +36,18 @@ elif pagina == "Control de Baile":
 audio_file = open("AudioBMO.mp3", "rb")
 audio_bytes = audio_file.read()
 
-if st.button("¡Reproducir Baile!"):
-    resultado = publicar_baile()
-    st.audio(audio_bytes, format="audio/mp3")
-    if resultado.rc == 0:
-        st.success("✅ Motores activados en Wokwi (mensaje MQTT enviado).")
-    else:
-        st.error("❌ Fallo al enviar el mensaje MQTT.")
+    if st.button("¡Reproducir Baile!"):
+        resultado = publicar_baile()
         st.audio(audio_bytes, format="audio/mp3")
-        st.success("Motores activados y música sonando.")
+        if resultado.rc == 0:
+            st.success("✅ Motores activados en Wokwi (mensaje MQTT enviado).")
+        else:
+            st.error("❌ Fallo al enviar el mensaje MQTT.")
+            st.audio(audio_bytes, format="audio/mp3")
+            st.success("Motores activados y música sonando.")
 
-# Página: Estado de Motores (ficticia para mostrar multipágina)
+# Página: Chatea con BMO
 elif pagina == "Chatea con BMO":
     st.title("Preguntale a BMO")
     st.write("Aquí podrías mostrar datos en tiempo real del sistema.")
+    #APARTIR DE AQUI EMPIEZA A PROGRAMAR EL SALUDO
